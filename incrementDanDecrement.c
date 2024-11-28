e0b5b68a1d65b599371c26f4df92c3ee2363dd85
Hendri Prasetyo <123240066@student.upnyk.ac.id>
2024-11-29 04:03:02 +0700

    Add files via upload

---
+++ incrementDanDecrement.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    x = 10;
+    printf("%d\n", x);
+    x += 2; // sama dengan x+2
+    printf("%d\n", x);
+    x -= 3; // sama dengan x-2
+    printf("%d\n", x);
+    x *= 2; // sama dengan x*2
+    printf("%d\n", x);
+    x %= 4; // sama dengan x%2
+    printf("%d\n", x);
+
+    //increment
+    printf("\nprogram increment\n");
+    x=1;
+    x++;
+    printf("%d\n", x);
+    x=1;
+    ++x;
+    printf("%d\n", x);
+
+    //decrement
+    printf("\nprogram decrement\n");
+    x=1;
+    x--;
+    printf("%d\n", x);
+    x=1;
+    --x;
+    printf("%d\n", x);
+
+    return 0;
+}
